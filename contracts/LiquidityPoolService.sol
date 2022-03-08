e5dfe3b3f89ad3f8a5ba61cb3f2396589127e5fc
Ewan Sheldon <ewansheldon@gmail.com>
2022-03-08 14:46:22 +0000

    switch to hard hat (for multiple compiler versions

---
+++ contracts/LiquidityPoolService.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.8.12;
+
+import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+contract LiquidityPoolService {
+    address constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+
+    address private tokenA;
+    address private tokenB;
+    IUniswapV2Factory factory;
+
+    constructor(address _tokenA, address _tokenB) public {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+        factory = IUniswapV2Factory(FACTORY);
+    }
+
+    function getPair() public view returns(IUniswapV2Pair) {
+        return IUniswapV2Pair(factory.getPair(tokenA, tokenB));
+    }
+
+    function getReserves() public view returns(uint reserveA, uint reserveB) {
+        return UniswapV2Library.getReserves(FACTORY, tokenA, tokenB);
+    }
+}
---
+++ contracts/SheldonToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SheldonToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    }
+}
