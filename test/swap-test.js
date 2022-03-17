48afdf71e7a28a22e56f8edeac9edfb47b328494
Ewan Sheldon <ewansheldon@gmail.com>
2022-03-17 23:13:56 +0000

    tests price changes

--- test/swap-test.js
+++ test/swap-test.js
@@ -1,14 +1,12 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
-const { utils } = require('web3');
-const { BN } = utils;
 
 describe('LiquidityPoolService', async () => {
   const INITIAL_SUPPLY = 1000000000000000;
   const ROUTER = "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a";
   let service, ewan, sheldon, liquidityProvider;
 
-  before(async () => {
+  beforeEach(async () => {
     [ owner, liquidityProvider ] = await ethers.getSigners();
     const EwanToken = await ethers.getContractFactory("EwanToken");
     const SheldonToken = await ethers.getContractFactory("SheldonToken");
@@ -21,41 +19,66 @@ describe('LiquidityPoolService', async () => {
     await service.deployed();
   });
 
-  it('initialises the pool with no liquidity', async () => {
-    const reserves = await service.getReserves();
-
-    expect(reserves.reserveA.toString()).to.equal("0");
-    expect(reserves.reserveB.toString()).to.equal("0");
+  describe('empty pool', async() => {
+    it('initialises the pool with no liquidity', async () => {
+      const reserves = await service.getReserves();
+  
+      expect(reserves.reserveA.toString()).to.equal("0");
+      expect(reserves.reserveB.toString()).to.equal("0");
+    });
+  
+    it('will not provide a quote when no liquidity', async () => {
+      try {
+        await service.quote(ewan.address, 50000);
+        throw new Error();
+      } catch (error) {
+        expect(error.message).contains("INSUFFICIENT_LIQUIDITY");
+      }
+  
+      try {
+        await service.quote(sheldon.address, 100000);
+        throw new Error();
+      } catch (error) {
+        expect(error.message).contains("INSUFFICIENT_LIQUIDITY");
+      }
+    });
   });
 
-  it('will not provide a quote when no liquidity', async () => {
-    try {
-      await service.quote(ewan.address, 50000);
-      throw new Error();
-    } catch (error) {
-      expect(error.message).contains("INSUFFICIENT_LIQUIDITY");
-    }
+  describe('initial liquidity', async () => {
+    const DESIRED_A = 50000;
+    const DESIRED_B = 50000;
+    const MIN_A = 50000;
+    const MIN_B = 50000;
+    const NOW_PLUS_MINUTE = Math.floor(Date.now() / 1000);
 
-    try {
-      await service.quote(sheldon.address, 100000);
-      throw new Error();
-    } catch (error) {
-      expect(error.message).contains("INSUFFICIENT_LIQUIDITY");
-    }
-  });
+    beforeEach(async () => {
+      await ewan.connect(owner).transfer(liquidityProvider.address, DESIRED_A);
+      await sheldon.connect(owner).transfer(liquidityProvider.address, DESIRED_B);
+      await ewan.connect(liquidityProvider).approve(service.address, DESIRED_A);
+      await sheldon.connect(liquidityProvider).approve(service.address, DESIRED_B);
+    });
 
-  it('allows you to add liquidity', async () => {
-    const desiredA = 50000;
-    const desiredB = 50000;
-    const minA = 50000;
-    const minB = 50000;
-    const nowPlusMinute = Math.floor(Date.now() / 1000);
-    await ewan.connect(owner).transfer(liquidityProvider.address, desiredA);
-    await sheldon.connect(owner).transfer(liquidityProvider.address, desiredB);
-    await ewan.connect(liquidityProvider).approve(service.address, desiredA);
-    await sheldon.connect(liquidityProvider).approve(service.address, desiredB);
-    await service.connect(liquidityProvider).addLiquidity(
-      desiredA, desiredB, minA, minB, liquidityProvider.address, nowPlusMinute
-    );
+    it('allows you to add liquidity', async () => {
+      await service.connect(liquidityProvider).addLiquidity(
+        DESIRED_A, DESIRED_B, MIN_A, MIN_B, liquidityProvider.address, NOW_PLUS_MINUTE
+      );
+      const reserves = await service.getReserves();
+  
+      expect(reserves.reserveA.toString()).to.equal(DESIRED_A.toString());
+      expect(reserves.reserveB.toString()).to.equal(DESIRED_B.toString());
+    });
+  
+    it('prices tokens as same value when equal reserves', async () => {
+      await service.connect(liquidityProvider).addLiquidity(
+        DESIRED_A, DESIRED_B, MIN_A, MIN_B, liquidityProvider.address, NOW_PLUS_MINUTE
+      );
+      const desiredSwapAmount = 10000;
+
+      const ewanPrice = await service.quote(ewan.address, desiredSwapAmount);
+      const sheldonPrice = await service.quote(sheldon.address, desiredSwapAmount);
+
+      expect(ewanPrice.toString()).to.equal(desiredSwapAmount.toString());
+      expect(sheldonPrice.toString()).to.equal(desiredSwapAmount.toString());
+    });
   });
 });
